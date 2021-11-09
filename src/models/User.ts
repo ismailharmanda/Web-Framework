12d711901fad82611ae598a74cacdb0598338497
ismail harmanda <iharmanda@hotmail.com>
2021-11-09 19:48:58 +0300

    test (retrieving User properties) success

--- src/models/User.ts
+++ src/models/User.ts
@@ -4,5 +4,9 @@ interface UserProps {
 }
 
 export class User {
-  constructor(private data: UserProps);
+  constructor(private data: UserProps) {}
+
+  get(propName: string): string | number {
+    return this.data[propName];
+  }
 }
