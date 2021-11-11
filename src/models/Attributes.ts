3efb99c4c03541a73afd8b2ee7886a7edd2806ea
ismail harmanda <iharmanda@hotmail.com>
2021-11-11 21:23:04 +0300

    An Advanced Generic Constraint

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,11 +1,23 @@
+import { UserProps } from "./User";
+
 export class Attributes<T> {
   constructor(private data: T) {}
 
-  get(propName: string): string | number {
-    return this.data[propName];
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
   }
 
   set(update: T): void {
     Object.assign(this.data, update);
   }
 }
+
+const attrs = new Attributes<UserProps>({
+  name: "İsmail Harmanda",
+  age: 29,
+  id: 10,
+});
+
+const name = attrs.get("name");
+const age = attrs.get("age");
+const id = attrs.get("id");
