15014307ddf98507b606bcb095f3c353157aa651
ismail harmanda <iharmanda@hotmail.com>
2021-11-11 14:42:52 +0300

    generic constraints around sync

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -1,16 +1,19 @@
 import axios, { AxiosPromise } from "axios";
-import { UserProps } from "./User";
 
 const apiUrl = "http://localhost:3000";
 const rootUrl = apiUrl + "/users";
 
-export class Sync {
+interface HasId {
+  id: number;
+}
+
+export class Sync<T extends HasId> {
   constructor(public rootUrl: string) {}
   fetch(id: number): AxiosPromise {
     return axios.get(`${this.rootUrl}/${id}`);
   }
 
-  save(data: UserProps): AxiosPromise {
+  save(data: T): AxiosPromise {
     const { id } = data;
     if (id) {
       // put
