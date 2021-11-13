cab5745fb4e4a4aadd7ec769cc15a156cc89738e
ismail harmanda <iharmanda@hotmail.com>
2021-11-14 02:21:25 +0300

    define Sync interface

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -1,10 +1,15 @@
+import { AxiosPromise } from "axios";
+
 interface ModelAttributes<T> {
   set(update: T): void;
   getAll(): T;
   get<K extends keyof T>(key: K): T[K];
 }
 
-interface Sync {}
+interface Sync<T> {
+  fetch(id: number): AxiosPromise;
+  save(data: T): AxiosPromise;
+}
 
 interface Events {
   on(eventName: string, callback: () => void): void;
