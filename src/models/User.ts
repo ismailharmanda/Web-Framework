0171e11db33b7177caa32294d5cf48608fbd33ee
ismail harmanda <iharmanda@hotmail.com>
2021-11-14 02:37:57 +0300

    refactor

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,7 @@
 import { Model } from "./Model";
+import { Attributes } from "./Attributes";
+import { ApiSync, rootUrl } from "./ApiSync";
+import { Eventing } from "./Eventing";
 
 export interface UserProps {
   id?: number;
@@ -6,4 +9,17 @@ export interface UserProps {
   age?: number;
 }
 
-export class User extends Model<UserProps> {}
+export class User extends Model<UserProps> {
+  static buildUser(attrs: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new ApiSync<UserProps>(rootUrl)
+    );
+  }
+}
+
+const user = User.buildUser({});
+user.get("id");
+user.get("name");
+user.get("age");
