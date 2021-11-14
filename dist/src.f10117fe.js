675d8054f8ae107cbe3866228023066cef41e566
ismail harmanda <iharmanda@hotmail.com>
2021-11-14 23:13:49 +0300

    Read input text and change name

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -134,6 +134,16 @@ function () {
     this.parent = parent;
     this.model = model;
 
+    this.onSetNameClick = function () {
+      var input = _this.parent.querySelector("input");
+
+      var name = input.value;
+
+      _this.model.set({
+        name: name
+      });
+    };
+
     this.onSetAgeClick = function () {
       _this.model.setRandomAge();
     };
@@ -151,12 +161,13 @@ function () {
 
   UserForm.prototype.eventsMap = function () {
     return {
-      "click:.set-age": this.onSetAgeClick
+      "click:.set-age": this.onSetAgeClick,
+      "click:.set-name": this.onSetNameClick
     };
   };
 
   UserForm.prototype.template = function () {
-    return "\n        <div>\n        <h1>User Form</h1>\n        <div>User name: " + this.model.get("name") + "</div>\n        <div>User age: " + this.model.get("age") + "</div>\n        <input />\n        <button>Click Me</button>\n        <button class=\"set-age\">Set Random Age</button>\n        </div>\n        ";
+    return "\n        <div>\n        <h1>User Form</h1>\n        <div>User name: " + this.model.get("name") + "</div>\n        <div>User age: " + this.model.get("age") + "</div>\n        <input />\n        <button class=\"set-name\">Change Name</button>\n        <button class=\"set-age\">Set Random Age</button>\n        </div>\n        ";
   };
 
   UserForm.prototype.bindEvents = function (fragment) {
